2dc4713c3c3955e951a7a6176ab617b019d77198
Jay Rodriguez <jared.m.rodriguez@gmail.com>
2020-07-13 17:42:22 -0500

    updates for auth

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const connectDB = require('./config/db');
 
+// const routes = require("./routes");
 const app = express();
 
 connectDB();
@@ -12,6 +13,7 @@ app.get('/', (req, res) => res.send('API Running'));
 
 //Define Routes
 app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/example', require('./routes/api/example'));
 
 const PORT = process.env.PORT || 5000;
