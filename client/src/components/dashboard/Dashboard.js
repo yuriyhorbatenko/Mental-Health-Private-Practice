58084691725387035e266ca73c97bf1da18698b5
Jay Rodriguez <jared.m.rodriguez@gmail.com>
2020-07-22 18:19:27 -0500

    updates to body

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -2,6 +2,7 @@ import React, { Fragment, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+// import Body from '../Body/index';
 import DashboardActions from './DashboardActions';
 import { getCurrentProfile, deleteAccount } from '../../actions/profile';
 
