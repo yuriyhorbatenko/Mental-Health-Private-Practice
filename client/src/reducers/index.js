64fdcdfe28ad19e095390665f85dd918d345bcf9
Jay Rodriguez <jared.m.rodriguez@gmail.com>
2020-07-22 20:22:51 -0500

    booking reducer index

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,11 +2,11 @@ import { combineReducers } from 'redux';
 import alert from './alert';
 import auth from './auth';
 import profile from './profile';
-import post from './booking';
+import booking from './booking';
 
 export default combineReducers({
   alert,
   auth,
   profile,
-  post,
+  booking,
 });
