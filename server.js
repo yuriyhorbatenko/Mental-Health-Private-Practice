c5152ad0bcef4e9145c86134246507c3e753df37
Jay Rodriguez <jared.m.rodriguez@gmail.com>
2020-07-25 12:43:24 -0500

    updates

--- server.js
+++ server.js
@@ -1,17 +1,15 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const routes = require('./routes');
+const PORT = process.env.PORT || 5000;
 
 const app = express();
-
-connectDB();
 
 //init middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json({ extended: false }));
 
 // Add routes, both API and view
-app.use(routes);
 
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
@@ -19,6 +17,8 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 }
 
-const PORT = process.env.PORT || 5000;
+app.use(routes);
+
+connectDB();
 
 app.listen(PORT, () => console.log('Server started on port' + PORT));
