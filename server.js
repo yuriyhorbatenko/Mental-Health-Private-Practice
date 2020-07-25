eca39b9b0800cf6b38c36251c4f9c694bc6a991d
Jay Rodriguez <jared.m.rodriguez@gmail.com>
2020-07-25 12:25:23 -0500

    updates to stuff 2

--- server.js
+++ server.js
@@ -4,24 +4,19 @@ const routes = require('./routes');
 
 const app = express();
 
-connectDB();
-
 //init middleware
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json({ exteneded: false }));
+app.use(express.json({ extended: false }));
 
 // Add routes, both API and view
-app.use(routes);
-
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
   app.use(express.static('client/build'));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
 }
+
+app.use(routes);
+connectDB();
 
 const PORT = process.env.PORT || 5000;
 
