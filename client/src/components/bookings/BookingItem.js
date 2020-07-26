cd917f487fab412779f3523c04710720288c9d82
Yuriy <xett03@gmail.com>
2020-07-26 16:44:14 -0400

    updated dashboard not final

--- client/src/components/bookings/BookingItem.js
+++ client/src/components/bookings/BookingItem.js
@@ -9,31 +9,31 @@ const BookingItem = ({
   booking: { booking, _id, text, appointmentDate, appointmentTime, user },
   showActions,
 }) => (
-  <Fragment>
-  <div className='booking bg-white p-1 my-1'>
-    <div>
-      {showActions &&  booking !== null &&(
+    <Fragment>
+      <div className='booking bg-white p-1 my-1'>
         <div>
-          {!auth.loading && user === auth.user._id && (
-            <>
-              <p className='my-1'>Appointment Date: {appointmentDate}</p>
-              <p className='my-1'>Appointment Time: {appointmentTime}</p>
-              <p className='my-1'>Comments: {text}</p>
-              <button
-                onClick={() => deleteBooking(_id)}
-                type='button'
-                className='btn btn-danger'
-              >
-                <i className='fas fa-times' />
-              </button>
-            </>
+          {showActions && booking !== null && (
+            <div>
+              {!auth.loading && user === auth.user._id && (
+                <>
+                  <p className='my-1'>Appointment Date: {appointmentDate}</p>
+                  <p className='my-1'>Appointment Time: {appointmentTime}</p>
+                  <p className='my-1'>Comments: {text}</p>
+                  <button
+                    onClick={() => deleteBooking(_id)}
+                    type='button'
+                    className='btn btn-danger'
+                  >
+                    <i className='fas fa-times' />
+                  </button>
+                </>
+              )}
+            </div>
           )}
+        </div>
       </div>
-      )}
-    </div>
-  </div>
-</Fragment>
-);
+    </Fragment>
+  );
 
 BookingItem.defaultProps = {
   showActions: true,
